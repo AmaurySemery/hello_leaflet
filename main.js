24666f5a2ded886c7e2ffd40e3974b2c4cb764b8
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-03-08 20:28:56 +0100

    Associer une icone personnalisée à un marker

--- main.js
+++ main.js
@@ -21,12 +21,20 @@ function init() {
         draggable: false
     }
 
+    const lilleIcone = {
+        lat: 50.63062636473414,
+        lng: 3.054455834909882,
+        title: 'Icone Lille',
+        draggable: false
+    }
+
     const zoomLevel = 12
 
     const map = L.map('mapid').setView([lille.lat, lille.lng], zoomLevel)
 
     addMarker(lille, map)
     addMarker(citadelleLille, map)
+    addCustomMarker(lilleIcone, map)
 
     const mainLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -49,6 +57,16 @@ function addMarker(options, map) {
         console.log('New coords: ', event.target._latlng)
         showNewCoords(event.target._latlng, event.target)
     })
+}
+
+function addCustomMarker(options, map)  {
+    const smileyIcon = L.icon({
+        iconUrl: './icons/oil-and-gas-well.png',
+        iconSize: [32, 37],
+        iconAnchor: [16, 18]
+    })
+    const marker = L.marker([options.lat, options.lng], { title: options.title, icon: smileyIcon })
+    marker.addTo(map)
 }
 
 function showNewCoords(coords, marker) {
